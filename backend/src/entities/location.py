cac630fa4e8121094292467ec184e0bd6725cf8a
Tim B <timbo95+github@posteo.de>
2020-05-14 22:04:53 +0200

    create class representing a location

--- backend/src/entities/location.py
+++ backend/src/entities/location.py
@@ -13,5 +13,23 @@ class Location(Entity, Base):
 
     geo_coordinate = Column(Geometry('POINT'))
     name = Column(String)
-    region = Column(String)
-    country = Column(String)
+    region = relationship(foreign_keys='regions.id')
+    country = relationship(foreign_keys='countries.id')
+
+    def __init__(self, geo_coordinate, name, region, country, created_by):
+        Entity.__init__(self, created_by)
+        self.geo_coordinate = geo_coordinate
+        self.name = name
+        self.region = region
+        self.country = country
+
+
+class LocationSchema(Schema):
+    id = fields.Number()
+    geo_coordinate = fields.String()
+    name = fields.String()
+    region = fields.Number()
+    country = fields.Number()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.String()
---
+++ backend/src/entities/region.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+from sqlalchemy import Column, String
+from marshmallow import Schema, fields
+from sqlalchemy.orm import relationship
+
+from .entity import Entity, Base
+
+
+class Region(Entity, Base):
+    __tablename__ = 'regions'
+
+    name = Column(String)
+    country = relationship(foreignkeys='countries.id')
+
+    def __init__(self, name, country, created_by):
+        Entity.__init__(self, created_by)
+        self.name = name
+        self.country = country
+
+
+class RegionSchema(Schema):
+    id = fields.Number()
+    name = fields.String()
+    country = fields.Number()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.String()
