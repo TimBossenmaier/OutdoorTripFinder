2a3e7c6bc198e51d5bd35b2768d7df351eb818b2
Tim B <timbo95+github@posteo.de>
2020-05-14 21:51:01 +0200

    create class representing a country

---
+++ backend/src/entities/country.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+from sqlalchemy import Column, String, Date, ARRAY, Integer
+from marshmallow import Schema, fields
+from sqlalchemy.orm import relationship
+
+from .entity import Entity, Base
+
+
+class Country(Entity, Base):
+    __tablename__ = 'countries'
+
+    name = Column(String)
+
+    def __init__(self, name, created_by):
+        Entity.__init__(self, created_by)
+        self.name = name
+
+
+class RegionSchema(Schema):
+    id = fields.Number()
+    name = fields.String()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.String()
