11d6164bbe4aa7dbfad3b968d7e3c9e759d500c3
Tim B <timbo95+github@posteo.de>
2020-05-18 21:29:17 +0200

    implement ForeignKey for activity_type

--- backend/src/entities/activity.py
+++ backend/src/entities/activity.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from sqlalchemy import Column, String,Text
+from sqlalchemy import Column, String, Text, Integer, ForeignKey
 from marshmallow import Schema, fields
 from sqlalchemy.orm import relationship
 from backend.src.entities.location_activity import LocationActivity
@@ -14,15 +14,15 @@ class Activity(Entity, Base):
     name = Column(String, nullable=False)
     locations = relationship('LocationActivity', uselist=True, backref='activities')
     description = Column(Text)
-    activity_type = Column(String, nullable=False)
+    activity_type_id = Column(Integer, ForeignKey('activity_type.id'), nullable=False)
     source = Column(String, nullable=False)
     save_path = Column(String, nullable=False)
 
-    def __init__(self, name, description, activity_type, source, save_path, created_by):
+    def __init__(self, name, description, activity_type_id, source, save_path, created_by):
         Entity.__int__(self, created_by)
         self.name = name
         self.description = description
-        self.activity_type = activity_type
+        self.activity_type_id = activity_type_id
         self.source = source
         self.save_path = save_path
 
@@ -31,7 +31,7 @@ class ActivitySchema(Schema):
     id = fields.Number()
     name = fields.String()
     description = fields.String()
-    activity_type = fields.String()
+    activity_type_id = fields.Number()
     source = fields.String()
     save_path = fields.String()
     created_at = fields.DateTime()
