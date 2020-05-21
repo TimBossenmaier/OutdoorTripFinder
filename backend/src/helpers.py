f60d7a400cbe262e9ae1fcfd252309a267361d39
Tim B <timbo95+github@posteo.de>
2020-05-21 20:54:01 +0200

    also add reversed conversion

--- backend/src/helpers.py
+++ backend/src/helpers.py
@@ -1,10 +1,14 @@
 import math
 
 
-def deg_to_radian(deg):
+def deg_to_radian(val, backwards=False):
     """
-    converts values in degree to radians
-    :param deg: value in degrees
+    converts values in degree to radians and vice versa
+    :param backwards: indicates the convertion to be in the other direction
+    :param val: value in degrees
     :return: converted value as radian value
     """
-    return deg * math.pi / 180
+    if not backwards:
+        return val * math.pi / 180
+    else:
+        return val * 180 / math.pi
