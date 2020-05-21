8cf03221d29ca313d9b3d0f11acb86c9ea916306
Tim B <timbo95+github@posteo.de>
2020-05-21 20:51:14 +0200

    little function to convert degrees into radians

---
+++ backend/src/helpers.py
@@ -0,0 +1,10 @@
+import math
+
+
+def deg_to_radian(deg):
+    """
+    converts values in degree to radians
+    :param deg: value in degrees
+    :return: converted value as radian value
+    """
+    return deg * math.pi / 180
