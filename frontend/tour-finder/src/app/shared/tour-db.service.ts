da8f3ae7c6f17db3484292f6488a1cb1fa16989b
Tim B <timbo95+github@posteo.de>
2020-08-28 15:54:09 +0200

    create service for communication with data base; initialize some tours for dev purpose

---
+++ frontend/tour-finder/src/app/shared/tour-db.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { Tour } from './tour';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TourDbService {
+tours: Tour[];
+
+  constructor() {
+    this.tours = [
+      {
+        id: '1',
+        name: 'Zwei-Taeler-Steig',
+        description: 'Auf dem Zwei-Täler-Steig im Naturpark Südschwarzwald lernen Wanderer zwei Gesichter der Region kennen.',
+        activity_type: 'Wandern',
+        source: 'Outdoor 04 2020',
+        save_path: 'Europa\\Deutschland\\Zwei_Taeler_Steig.pdf',
+        location: 'Waldkirch',
+        region: 'Schwarwald',
+        country: 'Deutschland',
+        distance: 20.5
+      },
+      {
+        id: '2',
+        name: 'Karwendel',
+        description: 'Stolze 125 Gipfel knacken im österreichisch-deutschen Karwendelgebirge die zweitausend-Meter-Marke - die meisten auf Tiroler Seite.',
+        activity_type: 'Wandern',
+        source: 'Outdoor 05 2020',
+        save_path: 'Europa\\Oesterreich\\Karwendel.pdf',
+        location: 'Rappenspitze',
+        region: 'Tirol',
+        country: 'Österreich',
+        distance: 152.3
+      }
+    ];
+  }
+}
