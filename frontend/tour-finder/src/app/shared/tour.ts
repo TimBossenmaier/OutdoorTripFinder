a1477cdd0a4bc6f1c92242e4f04a84449ae9b74c
Tim B <timbo95+github@posteo.de>
2020-08-28 15:34:03 +0200

    create description of a tour as interface

---
+++ frontend/tour-finder/src/app/shared/tour.ts
@@ -0,0 +1,10 @@
+export interface Tour {
+  id: string;
+  description?: string;
+  activity_type: string;
+  source: string;
+  save_path: string;
+  location: string;
+  region: string;
+  country: string;
+}
