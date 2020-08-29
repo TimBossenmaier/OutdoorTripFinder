bee8356e6131a285101b426f4187f48214a679ff
Tim B <timbo95+github@posteo.de>
2020-08-29 12:58:25 +0200

    create component for tour detail view

--- frontend/tour-finder/src/app/app.module.ts
+++ frontend/tour-finder/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TourDetailsComponent } from './tour-details/tour-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TourDetailsComponent
   ],
   imports: [
     BrowserModule,
