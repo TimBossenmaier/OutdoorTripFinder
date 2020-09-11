6ccc08ee1050a2e4c4a1cbddb7d24fb1d8cd4d46
Tim <bossenti@posteo.de>
2020-09-11 15:31:08 +0200

    add create function to region

--- backend/src/entities/region.py
+++ backend/src/entities/region.py
@@ -5,7 +5,7 @@ from marshmallow import Schema, fields
 from sqlalchemy.orm import relationship
 from backend.src.entities.country import Country
 
-from .entity import Entity, EntitySchema, Base
+from .entity import Entity, EntitySchema, Base, Session
 
 
 class Region(Entity, Base):
@@ -14,6 +14,12 @@ class Region(Entity, Base):
     name = Column(String, nullable=False)
     country_id = Column(Integer, ForeignKey('countries.id'), nullable=False)
     country = relationship(Country, foreign_keys=country_id)
+
+    def create(self):
+        session = Session()
+        session.add(self)
+        session.commit()
+        return self
 
     def __init__(self, name, country_id, created_by):
         Entity.__init__(self, created_by)
