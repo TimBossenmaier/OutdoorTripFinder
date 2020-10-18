ffc1af54afd0ae08d310cd63643cd5b1a786b2f4
Tim B <timbo95+github@posteo.de>
2020-10-18 17:39:29 +0200

    reqerite __init__.py

---
+++ backend/src/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from .config import config
+from .main import main as main_blueprint
+
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+
+    load_dotenv('../.env')
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    app.register_blueprint(main_blueprint)
+
+    return app
