af669a3b759985c03510ede71507f1477ef293ee
Tim <bossenti@posteo.de>
2020-10-20 20:41:55 +0200

    add endpoint to verify credentials

--- backend/app/auth/__init__.py
+++ backend/app/auth/__init__.py
@@ -1,3 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, make_response
+from ..entities.user import User
 
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/check_login', methods=['GET', 'POST'])
+def check_login():
+
+    data = request.get_json()
+
+    if data["username"]:
+        user = User.query.filter_by(username=data["username"]).first()
+    elif data["email"]:
+        user = User.query.filter_by(email=data["email"]).first()
+    else:
+        return make_response(jsonify('No username/email provided', 422))
+
+    if user is not None and user.password_hash == data["password_hash"]:
+        return make_response(jsonify(True, 201))
+    else:
+        return make_response(jsonify(False, 201))
