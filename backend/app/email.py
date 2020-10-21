19f5fed2535473a027b4d3e4c3f23aad876cda55
Tim <bossenti@posteo.de>
2020-10-21 16:01:29 +0200

    rework mail sending code

--- backend/app/email.py
+++ backend/app/email.py
@@ -1,19 +1,27 @@
-from threading import Thread
-from flask import current_app, render_template
-from flask_mail import Message
-from app.templates import mail
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from flask import current_app
 
 
 def send_email(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
-    msg = Message(app.config[' MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    return thr
+
+    msg = MIMEMultipart('alternative')
+    msg["Subject"] = current_app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject
+    msg["From"] = current_app.config['MAIL_SENDER']
+    msg["To"] = to
+
+    # TODO get html shown properly
+    body = open('app/templates/' + template + '.txt', 'r').read().format(**kwargs)
+    html = open('app/templates/' + template + '.html', 'r').read().format(**kwargs)
+
+    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(html, 'html'))
+
+    ssl_context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(current_app.config['MAIL_SERVER'],
+                          current_app.config['MAIL_PORT'],
+                          context=ssl_context) as server:
+        server.login(current_app.config["MAIL_USERNAME"], current_app.config["MAIL_PASSWORD"])
+        server.sendmail(current_app.config['MAIL_SENDER'], to, msg.as_string())
